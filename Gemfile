c4dd2442e47086c0dfa2e54d65211f79617bda8d
yizjiang <leo.jyz@gmail.com>
2014-05-03 14:51:09 -0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'mysql2'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+# To use ActiveModel has_secure_password
+gem 'bcrypt-ruby', '~> 3.0.0'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpotCard::Application.config.secret_token = '911876b308de76d51b7abfe5ee01be7aaa781f804dadcba0d5d97d626780e07bf9e7bba2dd6f7d7599686c46f4009f2a8ac1901a3362ba8d13e4b195d8477b60'
