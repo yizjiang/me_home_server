75446dff37c1bff379d94fff636898b8610f2ad5
yizjiang <leo.jyz@gmail.com>
2015-11-24 15:15:25 -0800

    migration script

--- db/migrate/20151124175859_add_more_to_home_cn.rb
+++ db/migrate/20151124175859_add_more_to_home_cn.rb
@@ -1,7 +1,11 @@
 class AddMoreToHomeCn < ActiveRecord::Migration
   def change
-    add_column :homes_cn, :short_desc, :text
-    add_column :homes_cn, :city, :string
+    unless column_exists? :homes_cn, :short_desc
+      add_column :homes_cn, :short_desc, :text
+    end
+    unless column_exists? :homes_cn, :city
+      add_column :homes_cn, :city, :string
+    end
     add_column :agent_extentions, :license_id, :string
   end
 end
