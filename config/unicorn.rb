f722766ea478fb6a729df6cb8d22d209a35180f6
meejia <palon.property@gmail.com>
2015-12-11 04:59:44 +0000

    unicorn.rb was overwrite by leo, cause server down

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,7 +7,7 @@ preload_app true
 
 timeout 60
 
-#listen "/var/run/unicorn/meejia/server.sock", :backlog => 64
-#pid "/var/run/unicorn/meejia/server.pid"
-#stderr_path "/tmp/unicorn/meejia/log/server.stderr.log"
-#stdout_path "/tmp/unicorn/meejia/log/server.stdout.log"
+listen "/var/run/unicorn/meejia/server.sock", :backlog => 64
+pid "/var/run/unicorn/meejia/server.pid"
+stderr_path "/tmp/unicorn/meejia/log/server.stderr.log"
+stdout_path "/tmp/unicorn/meejia/log/server.stdout.log"
