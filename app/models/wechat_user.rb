a60975b7809b0f0ab6ed6c1519039078ddeefa2f
yizjiang <leo.jyz@gmail.com>
2016-02-28 22:12:30 -0800

    send to wechat

--- app/models/wechat_user.rb
+++ app/models/wechat_user.rb
@@ -10,7 +10,6 @@ class WechatUser < ActiveRecord::Base
   }
 
   def send_homes_on_wechat(search = nil)
-    p 'ere'
     search ||= if search = self.search
                JSON.parse(search)
              else
@@ -37,15 +36,65 @@ class WechatUser < ActiveRecord::Base
     end
   end
 
+  def send_homes_to_wechat(zipcode, uid)
+    searches = [Search.new(regionValue: zipcode)]
+    homes = Home.search(searches)                        # should query id not equel
+    home_result(homes, uid)
+  end
 
-  def home_search_items(homes)
-    ticket = TicketGenerator.encrypt_uid(self.user_id)
-    homes.map do |home|
+  def set_redis(key, value, expired_time = 60)
+    REDIS.setex("#{self.open_id}:#{key}", expired_time, value.to_s)
+  end
+
+  def delete_redis(key)
+    REDIS.del("#{self.open_id}:#{key}")
+  end
+
+  def cached_input(type)
+    REDIS.get("#{self.open_id}:#{type}")
+  end
+
+  def home_result(homes, uid)
+    more_home = 0
+    if (homes.count > 0)
+      if homes.length > 10
+        more_home = homes.length - 9
+        showing_ids = homes[0..8].map(&:id)
+        set_redis('next_ids', (homes[9..-1].map(&:id)).join(','), 300)
+        homes = homes[0..8]
+      else
+        delete_redis('next_ids')
+      end
+
+      latest = homes.map { |h| h.last_refresh_at }.max + 1
+      #self.update_attributes(last_search: latest, search_count: (self.search_count || 0) + 1)
+      body = home_search_items(homes, more_home, uid)
+      WechatRequest.new.send_articles(to_user: self.open_id, body: body)
+    else
+      WechatRequest.new.send_text(to_user: self.open_id, body: '没有在售房源')
+    end
+  end
+
+
+
+  def home_search_items(homes, more_home = 0, uid)
+    ticket = TicketGenerator.encrypt_uid(uid)
+    homes = homes.map do |home|
       {title: "位于#{home.addr1} #{home.city}的 #{home.bed_num} 卧室 #{home.home_type}，售价：#{home.price}美金",
-       description: 'nice home',
-       picurl: "#{CDN_HOST}/photo/#{home.images.first.try(:image_url) || 'default.jpeg'}",
+       body: 'nice home',
+       pic_url: "#{CDN_HOST}/photo/#{home.images.first.try(:image_url) || 'default.jpeg'}",
        url: "#{CLIENT_HOST}/?ticket=#{ticket}#/home_detail/#{home.id}"}
     end
+
+    if more_home > 0
+      homes[homes.length] = {
+        title: "还有#{more_home}处房源, 请回复n或N查看下一页",
+        pic_url: "#{CDN_HOST}/photo/default.jpeg",
+        url: "#{CLIENT_HOST}/?ticket=#{ticket}#/"
+      }
+    end
+
+    homes
   end
 
 end
--- app/workers/search_worker.rb
+++ app/workers/search_worker.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class SearchWorker
   include Sidekiq::Worker
   sidekiq_options retry: 5
@@ -7,19 +9,19 @@ class SearchWorker
   end
 
   sidekiq_retries_exhausted do |msg|
-    p 'xxx'
-    p msg.inspect
-    WechatRequest.new.send_text(to_user: msg['args'][0], body: 'can not acquire location')
+    WechatRequest.new.send_text(to_user: msg['args'][0], body: '无法获取地址')
   end
 
   def perform(uid)
+    @uid = uid
     location = REDIS.get("#{uid}:location")
     raise 'location not set' unless location
     url = "http://dev.virtualearth.net/REST/v1/Locations/#{location}?o=json&includeEntityTypes=Postcode1&key=AjVrfYUU-6_5NnEHSjCxZ16XAJHyu0-J42p16WXCld6F52NujvxQ2iRV1X3UQeQs"
     response = Typhoeus.get(url)
     result = JSON.parse response.body
     zipcode = result['resourceSets'][0]['resources'][0]['address']['postalCode']
-    WechatUser.find_by_open_id(uid).send_homes_on_wechat('regionValue' => zipcode.to_s)
+    wuser = WechatUser.find_by_open_id(uid)
+    wuser.send_homes_to_wechat(zipcode, wuser.user.id)
     # do something
   end
 end
