89bf766e0ffb7ec9a3ac2668e4fb448880fbef52
cris89 <cris89@taou.com>
2016-06-18 21:54:41 +0800

    fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-root = "/Users/cris/me_home_server"
+root = "/Users/meejia/me_home_server"
 working_directory root
 
 worker_processes 2
@@ -6,8 +6,7 @@ worker_processes 2
 preload_app true
 
 timeout 60
-listen 8880
-#listen "/var/run/unicorn/meejia/server.sock", :backlog => 64
-#pid "/var/run/unicorn/meejia/server.pid"
+listen "/var/run/unicorn/meejia/server.sock", :backlog => 64
+pid "/var/run/unicorn/meejia/server.pid"
 stderr_path "/tmp/unicorn/meejia/log/server.stderr.log"
 stdout_path "/tmp/unicorn/meejia/log/server.stdout.log"
