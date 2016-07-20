02972c42b6d817520852855af3cd9ce7f28201a4
meejia <wenhl@yahoo.com>
2016-07-20 07:27:01 +0000

    fix date issue

--- app/models/agent_extention.rb
+++ app/models/agent_extention.rb
@@ -6,16 +6,15 @@ class AgentExtention < ActiveRecord::Base
 
   def self.importer(broker_agent)
     biz_license_id = broker_agent[7].nil? ? nil : broker_agent[7].lstrip.rstrip
-    p biz_license_id
     biz_name = broker_agent[0].nil? ? nil : broker_agent[0].lstrip.rstrip
     biz_addr = broker_agent[1].nil? ? nil : broker_agent[1].lstrip.rstrip
     biz_city = broker_agent[2].nil? ? nil : broker_agent[2].lstrip.rstrip
     biz_state = broker_agent[3].nil? ? nil : broker_agent[3].lstrip.rstrip
     biz_zipcode = broker_agent[4].nil? ? nil : broker_agent[4].lstrip.rstrip
     
-#    if (biz_license_id.nil? && biz_name.nil?)  
-#      return
-#    end 
+    if (biz_license_id.nil? && biz_name.nil?)  
+      return
+    end 
 
     if ((biz_zipcode.nil? || biz_zipcode.empty?)  && !(biz_addr.nil? || biz_addr.empty?))
       address = StreetAddress::US.parse(biz_addr)    #address = biz_addr.match(/^([^,]*),\s*(\w*)\s*(\d*)?$/)
@@ -28,26 +27,22 @@ class AgentExtention < ActiveRecord::Base
     end
 
     broker_company = BrokerCompany.where(license_id:biz_license_id).first
-    p broker_company   
     if (broker_company.nil? &&  !biz_name.nil? && !biz_zipcode.nil? && !biz_state.nil?)
       broker_company = BrokerCompany.where(name:biz_name, state:biz_state, zipcode:biz_zipcode).first
-      p 'did not find' if broker_company.nil?
       broker_company = BrokerCompany.new(:license_id => biz_license_id, :name => biz_name, :addr => biz_addr, :city => biz_city, :state => biz_state, :zipcode => biz_zipcode) if broker_company.nil? 
     end
     
     if (!broker_company.nil? )
-      p 'update broker'
+#      p 'update broker'
       broker_company.country = "USA"
       broker_company.phone = broker_agent[6].nil? ? broker_company.phone : broker_agent[6]
       broker_company.save
     end  
          
     if (!broker_company.nil?)
-     p 'create agent'
-     p broker_agent[18]         
+      p 'create agent'
       if (!broker_agent[18].nil? && broker_agent[18].match(/[-+]?[0-9]+/))
         biz_state = broker_company.state if biz_state.nil?
-        p biz_state
         agent = AgentExtention.where(license_id:broker_agent[18].lstrip.rstrip, license_state:biz_state).first
         agent = AgentExtention.new(:license_id => broker_agent[18].lstrip.rstrip, :license_state => biz_state) if agent.nil?    
         agent.first_name = broker_agent[8].nil? ? agent.first_name : broker_agent[8].lstrip.rstrip
@@ -61,12 +56,9 @@ class AgentExtention < ActiveRecord::Base
         agent.mail = broker_agent[14].nil? ? agent.mail : broker_agent[14].lstrip.rstrip
         agent.url = broker_agent[15].nil? ? agent.url : broker_agent[15].lstrip.rstrip
         agent.city_area = broker_agent[16].nil? ? agent.city_area : broker_agent[16].lstrip.rstrip 
-        p broker_agent[17].lstrip.rstrip
-        p (Time.now - broker_agent[17].lstrip.rstrip.to_i * 3600) 
-        p (Time.now - broker_agent[20].lstrip.rstrip.to_i * 3600) 
-        agent.license_issue = broker_agent[17].nil? ? agent.license_issue : (Time.now - broker_agent[17].lstrip.rstrip.to_i * 3600) unless broker_agent[17].nil?
+        agent.license_issue = broker_agent[17].nil? ? agent.license_issue : Date.strptime(broker_agent[17].lstrip.rstrip, "%m/%d/%y")
         agent.license_type = broker_agent[19].nil? ? agent.license_type : broker_agent[19].lstrip.rstrip
-        agent.license_expire = broker_agent[20].nil? ? agent_license_expire : (Time.now - broker_agent[20].lstrip.rstrip.to_i * 3600) unless broker_agent[20].nil?
+        agent.license_expire = broker_agent[20].nil? ? agent_license_expire : Date.strptime(broker_agent[20].lstrip.rstrip, "%m/%d/%y")
         agent.description = broker_agent[21].nil? ? agent.description : broker_agent[21].lstrip.rstrip
         agent.photo_url = broker_agent[22].nil? ? agent.photo_url : broker_agent[22].lstrip.rstrip[10..-5]
         agent.city_list = broker_agent[23].nil? ? agent.city_list : broker_agent[23].lstrip.rstrip
@@ -74,12 +66,9 @@ class AgentExtention < ActiveRecord::Base
         agent.mailing_address = broker_agent[25].nil? ? agent.source : broker_agent[25].lstrip.rstrip 
         agent.source = broker_agent[26].nil? ? agent.source : broker_agent[26].lstrip.rstrip 
         agent.source_id = broker_agent[27].nil? ? agent.source_id : broker_agent[27].lstrip.rstrip 
-        p agent.status
         agent.status = "pending" if  agent.status.nil?
         agent.broker_company_id = broker_company.id
-        p "before save"
         agent.save
-        p agent.id
       end 
     end     
   end
